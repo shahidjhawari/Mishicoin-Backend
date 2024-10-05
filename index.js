0e2da40acb3010ffbe617e33a78b23e25a42b3af
Shahid Iqbal <shahidiqbaljhawari@gmail.com>
2024-10-05 09:14:53 +0500

    latest

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
-require('dotenv').config();
-const express = require('express');
-const bodyParser = require('body-parser');
-const userRoutes = require('./routes/userRoutes');
-const connectDB = require('./db/connection');
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const userRoutes = require("./routes/userRoutes");
+const connectDB = require("./db/connection");
+const cors = require("cors");
 
 const app = express();
+
+app.use(cors());
 app.use(bodyParser.json());
 
-app.use('/api', userRoutes);
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "HEAD", "PATCH"],
+};
 
-const port = process.env.PORT || 5000; 
+app.use("/api", userRoutes);
+
+const port = process.env.PORT || 5000;
 
 connectDB()
   .then(() => {
@@ -17,7 +26,7 @@ connectDB()
       console.log(`Server is running on port ${port}`);
     });
   })
-  .catch(err => {
-    console.error('Failed to connect to MongoDB:', err);
+  .catch((err) => {
+    console.error("Failed to connect to MongoDB:", err);
     process.exit(1);
   });
