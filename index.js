94a1e2eed0df8cf7435905bcca149da080a4e19c
Shahid Iqbal <shahidiqbaljhawari@gmail.com>
2024-10-06 09:17:57 +0500

    cors added

--- index.js
+++ index.js
@@ -9,24 +9,24 @@ const app = express();
 
 // CORS configuration
 const corsOptions = {
-  origin: "http://localhost:3000",  // If React Native is on a different IP, replace localhost with that IP
-  credentials: true, 
+  origin: ["*"], // Allow requests from any origin
+  credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "HEAD", "PATCH"],
 };
 
-app.use(cors(corsOptions));  // Apply CORS with the defined options
-app.use(bodyParser.json());  // Parse incoming JSON payloads
+app.use(cors(corsOptions)); // Apply CORS with the defined options
+app.use(bodyParser.json()); // Parse incoming JSON payloads
 
 // API routes
-app.use("/api", userRoutes); 
+app.use("/api", userRoutes);
 
-app.get("/", (req, res) =>{
-  res.send("Welcome")
-})
+app.get("/", (req, res) => {
+  res.send("Welcome");
+});
 
-app.get("/home", (req, res) =>{
-  res.send("Welcome Home")
-})
+app.get("/home", (req, res) => {
+  res.send("Welcome Home");
+});
 
 // Set up the server port
 const port = process.env.PORT || 5000;
